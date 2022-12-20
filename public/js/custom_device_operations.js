f492e271fdabb4ccbbbf77e36c8bf055992fa103
Milan Nikolic <milan.nikolic@appsco.com>
2022-12-20 09:16:07 +0100

    Css styling

--- public/js/custom_device_operations.js
+++ public/js/custom_device_operations.js
@@ -5,6 +5,7 @@ window.addEventListener('DOMContentLoaded', () => {
     })
     document.querySelector('[data-action="requestUniqueIdentifier"]').addEventListener('request.response.body', (e) => {
         let resp = e.detail;
+        document.querySelector('[data-action="requestUniqueIdentifier"]').querySelector('[data-dpt-cab-id]').value = resp.deviceUniqueIdentifier;
         displayTableData(resp);
     })
 });
